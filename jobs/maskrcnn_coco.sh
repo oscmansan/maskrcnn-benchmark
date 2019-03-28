fb75101be0fa08022edf86523a96e4b537d23fce
Oscar Mañas <oscmansan@gmail.com>
2019-03-29 00:41:31 +0100

    Update maskrcnn_coco training job

--- jobs/maskrcnn_coco.sh
+++ jobs/maskrcnn_coco.sh
@@ -8,11 +8,12 @@
 #SBATCH --chdir /home/grupo06/.oscar/maskrcnn-benchmark
 #SBATCH --output ../logs/%x_%j.out
 
-source /home/grupo06/.oscar/maskrcnn-benchmark/venv/bin/activate
+source venv/bin/activate
 python tools/train_net.py --config-file configs/e2e_mask_rcnn_R_50_FPN_1x.yaml \
     SOLVER.IMS_PER_BATCH 4 \
     SOLVER.BASE_LR 0.005 \
     SOLVER.MAX_ITER 360000 \
     SOLVER.STEPS "(240000, 320000)" \
     TEST.IMS_PER_BATCH 2 \
-    MODEL.MASK_ON False
+    MODEL.MASK_ON False \
+    OUTPUT_DIR ../work/experiments/maskrcnn_coco_${SLURM_JOB_ID}
